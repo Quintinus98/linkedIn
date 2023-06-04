edbc89bb092714aae80b3c1b0b8f8fe99fc1a567
Obodo David <obododavidsop@gmail.com>
2023-06-04 11:31:52 +0100

    Updated Navbar, improved it's design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,37 +9,36 @@ function App() {
     "Consolidating and making reusable various react UI components from two user interfaces. Working and planning with a team to build open source projects";
 
   return (
-    <>
-      <div className="container">This is all test, to be deleted when pages are designed.</div>
+    <div>
+      <NavBar />
+      <div className="app">
+        <div className="container">This is all test, to be deleted when pages are designed.</div>
 
-      <div className="container">
-        <WorkExperience
-          title="Intern"
-          employmentType="Internship"
-          companyName="Outreachy"
-          location="Remote"
-          startDate={new Date(2022, 11, 5)}
-          endDate={new Date(2023, 2, 3)}
-          description={description}
-          skills={["patternfly", "Redux"]}
-        />
-        <Seperator />
-        <WorkExperience
-          title="Intern"
-          employmentType="Internship"
-          companyName="Outreachy"
-          location="Remote"
-          startDate={new Date(2022, 11, 5)}
-          endDate={new Date(2023, 2, 3)}
-          description={description}
-          skills={["patternfly", "Redux"]}
-        />
+        <div className="container">
+          <WorkExperience
+            title="Intern"
+            employmentType="Internship"
+            companyName="Outreachy"
+            location="Remote"
+            startDate={new Date(2022, 11, 5)}
+            endDate={new Date(2023, 2, 3)}
+            description={description}
+            skills={["patternfly", "Redux"]}
+          />
+          <Seperator />
+          <WorkExperience
+            title="Intern"
+            employmentType="Internship"
+            companyName="Outreachy"
+            location="Remote"
+            startDate={new Date(2022, 11, 5)}
+            endDate={new Date(2023, 2, 3)}
+            description={description}
+            skills={["patternfly", "Redux"]}
+          />
+        </div>
       </div>
-
-      <div className="container">
-        <NavBar />
-      </div>
-    </>
+    </div>
   );
 }
 
