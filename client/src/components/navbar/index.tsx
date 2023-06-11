e675c1e637eb43e47d89c2751cf184ab44e832b4
Obodo David <obododavidsop@gmail.com>
2023-06-11 20:47:13 +0100

    Added Nav_link border bottom

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -1,26 +1,24 @@
 import { profile } from "../../assets";
 import "./styles.css";
 import { ReactNode } from "react";
-import { useResolvedPath, useMatch, Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 interface customLinkProps {
   to: string;
   children: ReactNode;
 }
 
-const CustomLink = ({ to, children }: customLinkProps) => {
-  const resolvedPath = useResolvedPath(to);
-  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
-  return (
-    <>
-      <li className={isActive ? "active" : ""}>
-        <Link to={to} className="nav_link">
-          <div>{children}</div>
-        </Link>
-      </li>
-    </>
-  );
-};
+const CustomLink = ({ to, children }: customLinkProps) => (
+  <li>
+    <NavLink
+      to={to}
+      className={({ isActive, isPending }) =>
+        isPending ? "nav_link pending" : isActive ? "nav_link active" : "nav_link"
+      }>
+      <div>{children}</div>
+    </NavLink>
+  </li>
+);
 
 const NavBar = () => {
   return (
@@ -28,17 +26,19 @@ const NavBar = () => {
       <nav className="header">
         <div className="navbar">
           <div className="first">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              data-supported-dps="24x24"
-              fill="rgb(10, 102, 194)"
-              className="mercado-match"
-              width="41"
-              height="41"
-              focusable="false">
-              <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
-            </svg>
+            <NavLink to="/feed">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                data-supported-dps="24x24"
+                fill="rgb(10, 102, 194)"
+                className="mercado-match"
+                width="41"
+                height="41"
+                focusable="false">
+                <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
+              </svg>
+            </NavLink>
 
             <div className="nosubmit">
               <input type="search" name="Search" id="search" placeholder="Search" />
