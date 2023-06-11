e0dd2092755fc57ecafe4a983f5686c35b36052c
Obodo David <obododavidsop@gmail.com>
2023-06-11 09:04:36 +0100

    Updated Navlinks to use userdefined customlinks

--- client/src/pages/Jobs.tsx
+++ client/src/pages/Jobs.tsx
@@ -1,9 +1,7 @@
 // import React from 'react'
 
 const Jobs = () => {
-  return (
-    <div className="container">Jobs</div>
-  )
-}
+  return <div className="container">Jobs</div>;
+};
 
-export default Jobs
+export default Jobs;
