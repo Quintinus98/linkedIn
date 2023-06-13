9461267605fb529420ccdf2dc85c6c2d0f32419b
Obodo David <obododavidsop@gmail.com>
2023-06-13 21:54:00 +0100

    Updated img positioning

--- client/src/components/post/index.tsx
+++ client/src/components/post/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import { profile, Egbin } from "../../assets";
 import "./styles.css"
 
@@ -27,17 +27,17 @@ const Post = () => {
                 <path d="M8 1a7 7 0 107 7 7 7 0 00-7-7zM3 8a5 5 0 011-3l.55.55A1.5 1.5 0 015 6.62v1.07a.75.75 0 00.22.53l.56.56a.75.75 0 00.53.22H7v.69a.75.75 0 00.22.53l.56.56a.75.75 0 01.22.53V13a5 5 0 01-5-5zm6.24 4.83l2-2.46a.75.75 0 00.09-.8l-.58-1.16A.76.76 0 0010 8H7v-.19a.51.51 0 01.28-.45l.38-.19a.74.74 0 01.68 0L9 7.5l.38-.7a1 1 0 00.12-.48v-.85a.78.78 0 01.21-.53l1.07-1.09a5 5 0 01-1.54 9z"></path>
               </svg>
             </div>
-            <button>
-              <i className="fa-solid fa-ellipsis"></i>
-            </button>
           </div>
+          <button className="post__button">
+            <i className="fa-solid fa-ellipsis"></i>
+          </button>
         </div>
         <div className="post__text post--pad">
           "We are proud to be part of this great nation. Happy Democracy Day from all of us at Egbin
           Power Plc
         </div>
         <div className="post__content">
-          <img src={Egbin} alt="Egbin Power PLC" width={500} />
+          <img src={Egbin} alt="Egbin Power PLC"/>
         </div>
         <div className="post__comment-follow">
           <div>John Doe</div>
