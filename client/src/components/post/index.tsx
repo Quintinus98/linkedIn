81fdb459746b20914f407df54546e640f242255c
Obodo David <obododavidsop@gmail.com>
2023-06-15 05:14:47 +0100

    Completed update on Posts using dummy data

--- client/src/components/post/index.tsx
+++ client/src/components/post/index.tsx
@@ -1,6 +1,6 @@
 // import React from "react";
 import { profile, Egbin } from "../../assets";
-import "./styles.css"
+import "./styles.css";
 
 const Post = () => {
   return (
@@ -8,18 +8,18 @@ const Post = () => {
       <div className="post">
         <div className="post__header post--pad">
           <div className="post__logo">
-            <img src={profile} alt="" width={48} height={48} />
+            <img src={profile} alt="" />
           </div>
           <div className="post__profile">
             <h4>Egbin Power PLC</h4>
             <div>19850 followers</div>
-            <div>
-              5h •{" "}
+            <div className="post__profile--time">
+              5h<span style={{ color: "#666666" }}>•</span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 16 16"
                 data-supported-dps="16x16"
-                fill="currentColor"
+                fill="#666666"
                 className="mercado-match"
                 width="16"
                 height="16"
@@ -29,7 +29,17 @@ const Post = () => {
             </div>
           </div>
           <button className="post__button">
-            <i className="fa-solid fa-ellipsis"></i>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              data-supported-dps="24x24"
+              fill="currentColor"
+              className="mercado-match"
+              width="24"
+              height="24"
+              focusable="false">
+              <path d="M14 12a2 2 0 11-2-2 2 2 0 012 2zM4 10a2 2 0 102 2 2 2 0 00-2-2zm16 0a2 2 0 102 2 2 2 0 00-2-2z"></path>
+            </svg>
           </button>
         </div>
         <div className="post__text post--pad">
@@ -37,18 +47,70 @@ const Post = () => {
           Power Plc
         </div>
         <div className="post__content">
-          <img src={Egbin} alt="Egbin Power PLC"/>
+          <img src={Egbin} alt="Egbin Power PLC" />
         </div>
-        <div className="post__comment-follow">
+        <div className="post--pad post__comment-follow">
           <div>John Doe</div>
           <div>2 comments</div>
         </div>
-        <div className="post__reactions">
-          <div className="like"></div>
-          <div className="comment"></div>
-          <div className="repost"></div>
-          <div className="send"></div>
+        <div className="post--pad">
+          <hr />
         </div>
+
+        <ul className="post__reactions post--pad">
+          <li>
+            <button>
+              <div className="like ">
+                <i className="fa-regular fa-thumbs-up fa-flip-horizontal post__icon"></i>
+                <span>Like</span>
+              </div>
+            </button>
+          </li>
+          <li>
+            <button>
+              <div className="comment">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  data-supported-dps="24x24"
+                  fill="currentColor"
+                  className="mercado-match"
+                  width="24"
+                  height="24"
+                  focusable="false">
+                  <path d="M7 9h10v1H7zm0 4h7v-1H7zm16-2a6.78 6.78 0 01-2.84 5.61L12 22v-4H8A7 7 0 018 4h8a7 7 0 017 7zm-2 0a5 5 0 00-5-5H8a5 5 0 000 10h6v2.28L19 15a4.79 4.79 0 002-4z"></path>
+                </svg>
+                <span>Comment</span>
+              </div>
+            </button>
+          </li>
+          <li>
+            <button>
+              <div className="repost">
+                <i className="fa-solid fa-retweet post__icon"></i>
+                <span>Repost</span>
+              </div>
+            </button>
+          </li>
+          <li>
+            <button>
+              <div className="send">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  data-supported-dps="24x24"
+                  fill="currentColor"
+                  className="mercado-match"
+                  width="24"
+                  height="24"
+                  focusable="false">
+                  <path d="M21 3L0 10l7.66 4.26L16 8l-6.26 8.34L14 24l7-21z"></path>
+                </svg>
+                <span>Send</span>
+              </div>
+            </button>
+          </li>
+        </ul>
       </div>
     </>
   );
